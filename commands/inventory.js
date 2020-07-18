e3fa3fb064f0526cd9a8700116b52a4db9a23676
Baldeep Singh <38287537+itsbaldeep@users.noreply.github.com>
2020-07-18 22:43:26 +0530

    Typo fix

--- commands/inventory.js
+++ commands/inventory.js
@@ -51,7 +51,7 @@ module.exports.execute = async message => {
     }
 
     embed.addField(":toolbox: Potions", pots.length > 0 ? pots.sort().join('\n') : "Empty");
-    embed.addField(":birefcase: Items", items.length > 0 ? items.sort().join('\n') : "Empty");
+    embed.addField(":briefcase: Items", items.length > 0 ? items.sort().join('\n') : "Empty");
 
     message.channel.send(embed);
     return 1;
