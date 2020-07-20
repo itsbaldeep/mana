8a3ccea1ee335b808bde7529e76a4e2cbd4653e4
Baldeep Singh <38287537+itsbaldeep@users.noreply.github.com>
2020-07-20 21:48:40 +0530

    Tweaked a formula

--- formulas.js
+++ formulas.js
@@ -1,6 +1,6 @@
 module.exports.calculateMana = level => 70 + 28 * level + 10 * (level - 1);
 module.exports.calculateExp = (limit, level) => limit + 103 * level;
-module.exports.calculateMobMana = level => Math.floor(9 + 1.1 * level);
+module.exports.calculateMobMana = level => Math.floor(8 + 1.22 * level);
 module.exports.calculateMobExp = level => Math.floor(27 * level);
 module.exports.replenishMana = level => Math.floor(Math.random() * level * 6 + 50) + level;
 module.exports.exploreExp = level => Math.floor(Math.random() * level * 68) + Math.floor(56 * level);
