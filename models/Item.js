842a577d8d3af33125007429ea6cda665e5a170b
Baldeep Singh <38287537+itsbaldeep@users.noreply.github.com>
2020-07-30 18:39:10 +0530

    Useless file

--- models/Item.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Item", mongoose.Schema({
-    name: mongoose.Schema.Types.String,
-    rarity: mongoose.Schema.Types.Number
-}));
