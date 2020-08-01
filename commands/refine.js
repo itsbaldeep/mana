ec83ffabece98c524c0d4ee86a0ed1b518c1a540
Baldeep Singh <itsbaldeep@gmail.com>
2020-08-01 19:53:37 +0530

    Changes in refine command

--- commands/refine.js
+++ commands/refine.js
@@ -28,39 +28,29 @@ module.exports.execute = async message => {
     const name = pet.rarity + " Fragment";
     const frag = await Fragment.findOne({ name: name });
     let req = 0;
-    if (pet.rarity == "Common") req = 50;
-    if (pet.rarity == "Uncommon") req = 150;
-    if (pet.rarity == "Rare") req = 300;
-    if (pet.rarity == "Legendary") req = 600;
+    if (pet.rarity == "Common") req = 25;
+    if (pet.rarity == "Uncommon") req = 75;
+    if (pet.rarity == "Rare") req = 150;
+    if (pet.rarity == "Legendary") req = 300;
 
     // Validating requirements
     if (user.magicule < req || !user.fragments.has(frag._id.toString())) {
         message.channel.send(negative(message.author)
-            .addField(":name_badge: Unable to reroll", `**Fragment Required**: ${name}\n**Magicules Required**: ${req} magicules\n**Current Fragments**: ${user.fragments.get(frag._id.toString()) || 0} ${name}\n**Current Magicules**: ${user.magicule} magicules`)
+            .addField(":name_badge: Unable to refine", `**Fragment Required**: ${name}\n**Magicules Required**: ${req} magicules`)
         );
         return;
     }
 
-    // Taking magicules and fragments
-    user.magicule -= req;
-    remove(1, frag._id, user.fragments);
-
-    // Building message
-    const embed = positive(message.author)
-        .addFields(
-            { name: ":game_die: Fragments", value: `**Consumed**: ${name}\n**Current**: ${user.fragments.get(frag._id.toString()) || 0} ${name}` },
-            { name: ":gem: Magicules", value: `**Consumed**: ${req} magicules\n**Current**: ${user.magicule} magicules` }
-        );
-    
     // Applying new buffs
     const details = new Map();
     for (const key of user.buffs.keys()) {
+        // Getting buff and proc rate
         const buff = await Buff.findOne({ name: key });
         let proc = user.buffs.get(key);
+
+        // Setting old and new proc rates
         details.set(key, [proc]);
-        if (proc != buff.range.max) {
-            user.buffs.set(key, ++proc); 
-        }
+        if (proc != buff.range.max) user.buffs.set(key, ++proc); 
         details.set(key, details.get(key).concat([proc]));
     }
 
@@ -68,7 +58,17 @@ module.exports.execute = async message => {
     const data = [];
     for (const entry of details.entries())
         data.push(`**${entry[0]}**: ${entry[1][0]} -> ${entry[1][1]}`);
-    embed.addField(`:dart: ${pet.name} refined`, data.join("\n"));
+
+    // Building message
+    const embed = positive(message.author)
+        .addFields(
+            { name: `:dart: ${pet.name} refined`, value: data.join("\n") },
+            { name: ":slot_machine: Details", value: `**${name}**: ${user.fragments.get(frag._id.toString())} - 1 = ${user.fragments.get(frag._id.toString()) - 1}\n**Magicules**: ${user.magicule} - ${req} = ${user.magicule - req}` }
+        );
+    
+    // Taking magicules and fragments
+    user.magicule -= req;
+    remove(1, frag._id, user.fragments);
 
     // Updating user and sending message
     await User.updateOne({ id: message.author.id }, { $set: user });
